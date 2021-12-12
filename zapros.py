988a2615b011e7a3150951c9817c97a6ece309fa
ShompolovMaxim <Smax2204@yandex.ru>
2021-12-12 15:02:52 +0300

    12.02.21

--- zapros.py
+++ zapros.py
@@ -1,68 +1,58 @@
 import sqlite3
-conn = sqlite3.connect('DataBase.db')
-cursor = conn.cursor()
 
-#1
+class DB:
+    def __init__(self,path):
+        self.connection = sqlite3.connect(path)
 
-def ReadyOrders():
-    cursor.execute(
-        '''
-        SELECT 
-            NumberOfOrder,
-            (SELECT GoodID FROM Prices WHERE ID=PriceID) AS GoodID,
-            (SELECT Price FROM Prices WHERE ID=PriceID) AS Price,
-            Quantity,
-            CustomerID
-            FROM Orders WHERE Status = 'ReadyToByDelivered';
-        '''
-    )
-    print(cursor.fetchall())
+    def newCursor(self):
+        self.cursor = self.connection.cursor()
+    
+    #1
+    def ReadyOrders(self):
+        self.cursor.execute(
+            '''
+            SELECT 
+                NumberOfOrder,
+                (SELECT GoodID FROM Prices WHERE ID=PriceID) AS GoodID,
+                (SELECT Price FROM Prices WHERE ID=PriceID) AS Price,
+                Quantity,
+                CustomerID
+                FROM Orders WHERE Status = 'ReadyToByDelivered';
+            '''
+        )
+        return self.cursor.fetchall()
 
-#2
-def UpdatePrices():
-    cursor.execute(
-        '''
-        UPDATE Goods SET PriceID=(SELECT ID FROM Prices WHERE GoodID=Goods.ID and EndDate>=(SELECT DATE('now')) and StartDate<=(SELECT DATE('now')))
-        WHERE (SELECT EndDate FROM Prices WHERE Goods.PriceID=Prices.ID)<(SELECT DATE('now'));
-        '''
-    )
+    #2
+    def UpdatePrices(self):
+        self.cursor.execute(
+            '''
+            UPDATE Goods SET PriceID=(SELECT ID FROM Prices WHERE GoodID=Goods.ID and EndDate>=(SELECT DATE('now')) and StartDate<=(SELECT DATE('now')))
+            WHERE (SELECT EndDate FROM Prices WHERE Goods.PriceID=Prices.ID)<(SELECT DATE('now'));
+            '''
+        )
 
-#3
+    #3
+    def CategoryGoods(self,category):
+        self.cursor.execute(
+            '''
+            SELECT
+                Name,
+                Quantity, 
+                Description,
+                (SELECT Price FROM Prices WHERE Prices.ID=PriceID)AS Price,
+                (SELECT EndDate FROM Prices WHERE Prices.ID=PriceID)AS EndDate
+                FROM Goods WHERE Goods.СategoryID=?;
+            ''',(category,)
+        )
+        return self.cursor.fetchall()
 
-def CategoryGoods():
-    cursor.execute(
-        '''
-        SELECT
-            Name,
-            Quantity, 
-            Description,
-            (SELECT Price FROM Prices WHERE Prices.ID=PriceID)AS Price,
-            (SELECT EndDate FROM Prices WHERE Prices.ID=PriceID)AS EndDate
-            FROM Goods WHERE Goods.СategoryID=2;
-        '''
-    )
-    print(cursor.fetchall())
+path='DataBase.db'
 
-#4
-def CategoryInformation():
-    cursor.execute('SELECT name, Description FROM Categories WHERE ID=1')
-    print(cursor.fetchall())
-
-#5
-def UpdateOrder():
-    cursor.execute("UPDATE Orders SET Status='Delivered' WHERE NumberOfOrder=1;")
-
-#6
-def ReduceQuantity():
-    cursor.execute("UPDATE Goods SET Quantity=Quantity-5 WHERE ID = 1;")
-
-
-'''ReadyOrders()
-CategoryGoods()
-CategoryInformation()'''
-#UpdatePrices()
-#UpdateOrder()
-ReduceQuantity()
-conn.commit()
-conn.close()
+db=DB(path)
+db.newCursor()
+#print(db.ReadyOrders())
+#print(db.CategoryGoods(2))
+#DB.UpdatePrices()
+'''db._connection'.commit()
+db._connection'.close()'''
 
