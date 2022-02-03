b6596a4fd947e4ed2d1018ddec70d2a1d2941ad3
ShompolovMaxim <Smax2204@yandex.ru>
2022-02-03 12:22:03 +0300

    1

---
+++ Project/main.py
@@ -0,0 +1,28 @@
+from flask import *
+
+app = Flask(__name__)
+
+feedback=[]
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+@app.route('/about_us/')
+def about_us():
+    if request.method == 'POST':
+        review = request.form.get('review')
+        feedback.appnend(review)
+        print(feedback)
+    return render_template('about_us.html')
+
+@app.route('/catalog/')
+def catalog():
+    return render_template('Catalog.html')
+
+@app.route('/product1/')
+def product1():
+    return render_template('product.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
